c2dc8eb8580bf5f4227fbe813aaa411012ceef10
KaioGalvao <kaiovsagalvao@gmail.com>
2022-08-17 09:12:27 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ExemploAspNetMvc.Models;
+
+namespace ExemploAspNetMvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult PrimeiraAction()
+    {
+        return View();
+    }
+
+    public string TesteQueryString([FromQuery]string q, [FromQuery]string nome)
+    {
+        return $"Chegou aqui {q} para {nome}";
+    }
+
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    public string TesteForm([FromForm]string nome, [FromForm]string email)
+    {
+        return $"nome: {nome} email: {email}";
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
